467a4d886bd6cd4fb80f5166f1b23c81d695911c
Mahmoud Tarek <mah.algohry@gmail.com>
2023-11-28 21:31:21 +0400

    prepare the project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
